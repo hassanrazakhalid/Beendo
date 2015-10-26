95ff65b8dfece42c36d43c03d7942c36ddbce5b9
hassanrazakhalid <hassanrazakhalid@yahoo.com>
2015-10-27 01:59:15 +0500

    ..

--- src/main/java/com/Beendo/HibernateUtils/HibernateUtil.java
+++ src/main/java/com/Beendo/HibernateUtils/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-
+ 
 public class HibernateUtil {
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
